f0558aa675d89cab257e6d4d8449da3246dc68bc
Luis del Amo <luisdelamo@gmail.com>
2023-08-19 00:37:33 +0200

    jest config

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,15 @@
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
+	preset: 'ts-jest',
+	testEnvironment: 'node',
+	verbose: true, // con esta opción vemos los resultados individual de los test
+	collectCoverage: true,
+	coverageDirectory: './coverage',
+	coverageThreshold: {
+		global: {
+			statements: 100,
+			branches: 100,
+			functions: 100,
+			lines: 100,
+		},
+	},
 };
